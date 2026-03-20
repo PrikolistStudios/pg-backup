446081c5be0941e6ea0519df9546a88d8a1bb5cd
Roman Kabirov <roman.kabirov.2003@mail.ru>
2026-03-20 15:33:24 +0300

    add cobra project base

---
+++ internal/cmd/root.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "pg-backup",
+	Short: "Removal and backup of PostgreSQL databases",
+	Long: `This CLI can be used for easy removal and backup of PostgreSQL databases. 
+It supports action on multiple databases in one command and globbind`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 0 && cmd.Flags().NFlag() == 0 {
+			cmd.Help()
+			os.Exit(0)
+		}
+	},
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ prog/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/PrikolistStudios/pg-backup/internal/cmd"
+
+func main() {
+	cmd.Execute()
+}
